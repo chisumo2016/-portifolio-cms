37203f2c093b2feef1c4c3d3eb505ce740f0219e
Bernard Chisumo <bchisumo74@gmail.com>
2020-05-10 21:06:24 +0100

    Implemented the command user create

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:create-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a user for the admin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $first_name = $this->ask("What is the user\'s       first name?");
+        $last_name = $this->ask("What is the user\'s       last name?");
+        $email = $this->ask("What is the user\'s       email ?");
+        $twitter_handle = $this->ask("What is the user\'s   twitter username ?");
+
+        $password = $this->secret("What is the user\'s   password ?");
+        $passwordConfirm = $this->secret("Please Confirm  the user\'s   password ?");
+
+        if ($password != $passwordConfirm) {
+            return $this->error('Passwords do not match');
+        }
+        $this->table([
+            'First Name',
+            'Last Name',
+            'Email',
+            'Twitter Username'
+        ],
+            [
+                [
+                    $first_name,
+                    $last_name,
+                    $email,
+                    $twitter_handle
+                ]
+            ]);
+        if ($this->confirm("Does everything look right ?")) {
+            User::create([
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email,
+                'twitter_handle' => $twitter_handle,
+                'password' => Hash::make($password),
+                'role' => User::ADMIN_ROLE,
+
+            ]);
+            
+            return $this->info("User created");
+        }
+            return $this->info("User not created");
+    }
+}
